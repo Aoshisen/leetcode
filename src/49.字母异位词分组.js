bf2641a6ddd465477fa22ea3b0e7e21c0de73f93
aoshisen <aoshisen.23@gmail.com>
2025-07-10 21:14:44 +0800

    feat: 字母异位词分组

--- src/49.字母异位词分组.js
+++ src/49.字母异位词分组.js
@@ -5,7 +5,18 @@
 import { registerMatchers } from "../test/utils"
 
 var groupAnagrams = function (strs) {
-	return [["nat", "tan"], ["bat"], ["ate", "eat", "tea"]]
+	const map = new Map()
+	for (let index = 0; index < strs.length; index++) {
+		const current = strs[index];
+		const key = [...current].sort().join("");
+		const value = map.get(key);
+		if (!value) {
+			map.set(key, [current])
+		} else {
+			map.set(key, [...value, current])
+		}
+	}
+	return Array.from(map.values())
 };
 
 if (import.meta.vitest) {
@@ -15,6 +26,18 @@ if (import.meta.vitest) {
 	it("case1", () => {
 		const strs = ["eat", "tea", "tan", "ate", "nat", "bat"];
 		const result = [["bat"], ["nat", "tan"], ["ate", "eat", "tea"]]
+		expect(groupAnagrams(strs)).toEqualUnordered(result);
+	})
+
+	it("case2", () => {
+		const strs = [""];
+		const result = [[""]]
+		expect(groupAnagrams(strs)).toEqualUnordered(result);
+	})
+
+	it("case2", () => {
+		const strs = ["a"];
+		const result = [["a"]]
 		expect(groupAnagrams(strs)).toEqualUnordered(result);
 	})
 

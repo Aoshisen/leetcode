62fd5a0b17b07416ca59ffaf91757f6c778eeede
aoshisen <aoshisen.23@gmail.com>
2025-07-13 21:58:49 +0800

    feat: 双指针优化三数之和

--- src/15. 三数之和.js
+++ src/15. 三数之和.js
@@ -1,16 +1,63 @@
-/**
- * @param {number[]} nums
- * @return {number[][]}
- */
-var threeSum = function (nums) {
+import { registerMatchers } from "../test/utils"
 
-	return [[-1, -1, 2, -1, 0, 1]]
+var threeSum = function (nums) {
+	const result = [];
+	nums.sort((a, b) => a - b); // 先对数组进行排序
+
+	for (let i = 0; i < nums.length - 2; i++) {
+		// 跳过重复的元素
+		if (i > 0 && nums[i] === nums[i - 1]) continue;
+
+		let left = i + 1;
+		let right = nums.length - 1;
+
+		while (left < right) {
+			const sum = nums[i] + nums[left] + nums[right];
+
+			if (sum === 0) {
+				result.push([nums[i], nums[left], nums[right]]);
+				// 跳过重复的元素
+				while (left < right && nums[left] === nums[left + 1]) left++;
+				while (left < right && nums[right] === nums[right - 1]) right--;
+				left++;
+				right--;
+			} else if (sum < 0) {
+				left++;
+			} else {
+				right--;
+			}
+		}
+	}
+
+	return result;
 };
+// var threeSum = function (nums) {
+// 	const result = []
+// 	for (let i = 0; i < nums.length; i++) {
+// 		const i_num = nums[i];
+// 		for (let j = i + 1; j < nums.length; j++) {
+// 			const j_num = nums[j];
+// 			for (let k = j + 1; k < nums.length; k++) {
+// 				const k_num = nums[k];
+// 				if (i_num + k_num + j_num === 0) {
+// 					const current = [i_num, j_num, k_num]
+// 					if (result.some(item => item.includes(i_num) && item.includes(k_num) && item.includes(j_num))) {
+// 						continue;
+// 					}
+// 					result.push(current)
+// 				}
+// 			}
+
+// 		}
+// 	}
+// 	return result;
+// };
+
 if (import.meta.vitest) {
 	const { it, expect, beforeAll } = import.meta.vitest
 	beforeAll(() => { registerMatchers() })
 
-	it.only("case1", () => {
+	it("case1", () => {
 		const input = [-1, 0, 1, 2, -1, -4];
 		const result = [[-1, -1, 2], [-1, 0, 1]]
 		expect(threeSum(input)).toEqualUnordered(result);

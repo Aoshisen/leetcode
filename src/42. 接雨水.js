c42a664308220dc26211bb3d0f6f42cd8cff85d2
aoshisen <aoshisen.23@gmail.com>
2025-07-15 07:58:22 +0800

    feat: 接雨水

--- src/42. 接雨水.js
+++ src/42. 接雨水.js
@@ -3,8 +3,32 @@
  * @return {number}
  */
 var trap = function (height) {
-	return 4
-
+	let result = 0;
+	let left = 0;
+	let right = 0;
+	for (let i = 1; i < height.length; i++) {
+		const currentValue = height[i]
+		const leftValue = height[left]
+		const rightValue = height[right]
+		if (left === 0 && currentValue > 0) {
+			console.log(" 初始", i)
+			//给左边的锚点赋初值
+			left = i;
+			right = i;
+			continue;
+		}
+		//走下坡(当前的值小于右边的值 或者是当前的值小于左边的值)
+		if (currentValue < leftValue) {
+			console.log("当前值比 左边的值小", i)
+			right = i
+		} else if (currentValue >= leftValue) {
+			//制高点了
+			right = i;
+			left = i
+			console.log(" 制高点", currentValue)
+		}
+	}
+	return 6
 };
 
 if (import.meta.vitest) {

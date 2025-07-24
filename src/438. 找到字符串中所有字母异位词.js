04f3037769a12a69650bc141aad6fc02f6e1e4b4
aoshisen <aoshisen.23@gmail.com>
2025-07-24 23:17:28 +0800

    feat: 使用数组来 快速查找

--- src/438. 找到字符串中所有字母异位词.js
+++ src/438. 找到字符串中所有字母异位词.js
@@ -3,23 +3,63 @@
  * @param {string} p
  * @return {number[]}
  */
-// 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+// // 给定两个字符串 s 和 p，找到 s 中所有 p 的 异位词 的子串，返回这些子串的起始索引。不考虑答案输出的顺序。
+// var findAnagrams = function (s, p) {
+// 	let result = [];
+// 	const sArray = s.split('');
+// 	const pArray = p.split("").sort();
+// 	console.log(pArray, "<<<<<<<")
+// 	for (let i = 0; i < sArray.length - (pArray.length - 1); i++) {
+// 		const currentArray = sArray.slice(i, i + pArray.length);
+// 		console.log(currentArray);
+// 		const isInPArray = currentArray.sort().every((item, i) => item === pArray[i]);
+// 		if (isInPArray) {
+// 			result.push(i)
+// 		}
+// 	}
+// 	return result;
+// };
 var findAnagrams = function (s, p) {
-	let result = [];
-	const sArray = s.split('');
-	const pArray = p.split("").sort();
-	console.log(pArray, "<<<<<<<")
-	for (let i = 0; i < sArray.length - (pArray.length - 1); i++) {
-		const currentArray = sArray.slice(i, i + pArray.length);
-		console.log(currentArray);
-		const isInPArray = currentArray.sort().every((item, i) => item === pArray[i]);
-		if (isInPArray) {
-			result.push(i)
+	const result = [];
+	const pCount = new Array(26).fill(0); // 记录 p 中每个字符的频率
+	const sCount = new Array(26).fill(0); // 记录当前窗口中每个字符的频率
+	const aCharCode = 'a'.charCodeAt(0);
+
+	// 初始化 p 的字符频率
+	for (const char of p) {
+		pCount[char.charCodeAt(0) - aCharCode]++;
+	}
+
+	const pLength = p.length;
+	const sLength = s.length;
+
+	// 从右到左滑动窗口
+	for (let i = 0; i < sLength; i++) {
+		sCount[s[i].charCodeAt(0) - aCharCode]++;
+
+		// 移除窗口左侧多余的字符
+		if (i >= pLength) {
+			sCount[s[i - pLength].charCodeAt(0) - aCharCode]--;
+		}
+
+		// 比较窗口和 p 的字符频率
+		if (arraysEqual(sCount, pCount)) {
+			result.push(i - pLength + 1); // 起始索引为当前索引减去窗口长度加 1
 		}
 	}
+
 	return result;
 };
 
+// 辅助函数：比较两个数组是否相等
+function arraysEqual(arr1, arr2) {
+	for (let i = 0; i < arr1.length; i++) {
+		if (arr1[i] !== arr2[i]) {
+			return false;
+		}
+	}
+	return true;
+}
 if (import.meta.vitest) {
 	const { it, expect } = import.meta.vitest
 	it("case1", () => {

9f4fe6db75bd39de73b7fcc2c3a02deeab11afd7
aoshisen <aoshisen.23@gmail.com>
2025-07-28 22:12:39 +0800

    feat: 滑动窗口最大值

---
+++ src/239. 滑动窗口最大值.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var maxSlidingWindow = function (nums, k) {
+	let result = [];
+	let currentMax = -Infinity;
+	for (let i = 0; i < nums.length; i++) {
+		const current = nums[i];
+		currentMax = Math.max(currentMax, current);
+		if (i >= k - 1) {
+			result.push(Math.max(currentMax, current))
+		}
+	}
+	return result;
+
+};
+if (import.meta.vitest) {
+	const { it, expect } = import.meta.vitest
+	it("case1", () => {
+		const nums = [1, 3, -1, -3, 5, 3, 6, 7], k = 3
+		const result = [3, 3, 5, 5, 6, 7]
+		expect(maxSlidingWindow(nums, k)).toStrictEqual(result)
+	})
+
+	it("case2", () => {
+		const nums = [1], k = 1
+		const result = [1]
+		expect(maxSlidingWindow(nums, k)).toStrictEqual(result)
+	})
+
+}
